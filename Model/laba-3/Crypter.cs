b0dde5390f27b3300f765b8ae1e0be522a1dfda9
SharTrest <74322735+SharTrest@users.noreply.github.com>
2023-09-23 11:18:06 +0300

    lite ver

---
+++ Model/laba-3/Crypter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplom.Client.Model.laba_3
+{
+    internal class Crypter
+    {
+    }
+}
---
+++ Model/laba-3/Decrypter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplom.Client.Model.laba_3
+{
+    public class Decrypter
+    {
+        public string Password { get; set; }
+        public string FileName { get; set; }
+
+        public Decrypter()
+        {
+            FileName = "DeDataBase.txt";
+        }
+    }
+}
